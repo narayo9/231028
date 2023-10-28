751b169da397a38d3f76a7c55c848bcd7de09452
Chanuk Kim <cksdnr987@gmail.com>
2023-10-28 16:34:40 +0900

    post 생성

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import PostCreateView
+from blog.views import PostCreateView, PostListView
 
 urlpatterns = [
     path('posts/create/', PostCreateView.as_view(), name='post_create'),
+    path('posts/', PostListView.as_view(), name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,22 @@ from django.views.generic import ListView, DetailView, CreateView
 from blog.forms import PostForm
 from blog.models import Post
 from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 
 class PostCreateView(CreateView):
     template_name = "post_create.html"
     model = Post
     form_class = PostForm
+    success_url = reverse_lazy("post_list")
 
     def form_valid(self, form):
-        instance = form.save(commit=False)
-        instance.created_by = self.request.user
-        instance.save()
-        return HttpResponseRedirect(self.get_success_url())
+        self.object = form.save(commit=False)
+        self.object.created_by = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class PostListView(ListView):
+    template_name = "post_list.html"
+    model = Post
