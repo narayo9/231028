05876f63922f826b46762e7bcb8f3da80a279408
Chanuk Kim <cksdnr987@gmail.com>
2023-10-28 16:27:00 +0900

    포스트 생성 기능

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.views.generic import ListView, DetailView, CreateView
 
+from blog.forms import PostForm
 from blog.models import Post
+from django.http import HttpResponseRedirect
 
 
 class PostCreateView(CreateView):
     template_name = "post_create.html"
     model = Post
-    fields = ["title", "description", "created_by"]
+    form_class = PostForm
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.created_by = self.request.user
+        instance.save()
+        return HttpResponseRedirect(self.get_success_url())
