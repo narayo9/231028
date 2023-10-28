ccf6798f51c828344819715b38e7bce0b4d84fbe
Chanuk Kim <cksdnr987@gmail.com>
2023-10-28 16:40:10 +0900

    post 상세 연결

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import PostCreateView, PostListView
+from blog.views import PostCreateView, PostListView, PostDetailView
 
 urlpatterns = [
     path('posts/create/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('posts/', PostListView.as_view(), name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -22,3 +22,8 @@ class PostCreateView(CreateView):
 class PostListView(ListView):
     template_name = "post_list.html"
     model = Post
+
+
+class PostDetailView(DetailView):
+    template_name = "post_detail.html"
+    model = Post
